f38f1d2265a276e4e6db41d848f2e7c771a44c66
Steffen Plunder <steffen.plunder@web.de>
2024-11-18 18:14:04 +0900

    init

---
+++ 001_intro_to_julia.jl
@@ -0,0 +1,26 @@
+#= 
+
+    A quick introduction to the Julia programming language for scientific modelling.
+
+=# 
+
+
+#= 1. 
+
+    Why Julia 
+    - Julia is a high-level, high-performance, dynamic programming language.
+    - Julia is designed for numerical and scientific computing.
+    - Julia is open source and free to use.
+    - Julia has a simple and easy-to-learn syntax.
+
+    All programming languages are cool! And especially python/MATLAB have proven 
+    to be useful! 
+
+    However. Both suffer from the two language problem: 
+    - high level code is written in python 
+    - high performance code relies on C/C++ implementations (numpy, pytorch, etc)
+
+    The aim was to make both possible in one language. -> Julia!
+=# 
+
+
---
+++ 002_first_example.jl
@@ -0,0 +1,65 @@
+using DifferentialEquations, Plots
+
+# 1. Solving an ODE model with Julia 
+function lotka_volterra!(du, u, p, t)
+    (α, β, γ, δ) = p 
+    (x, y) = u
+
+    du[1] =  α * x - β * x * y
+    du[2] = -γ * y + β * x * y 
+end
+
+u0 = [1.0, 1.0]
+tspan = (0.0, 10.0)
+p = [1.5, 1.0, 3.0, 1.0]
+
+prob = ODEProblem(lotka_volterra!, u0, tspan, p)
+sol = solve(prob, Tsit5())
+plot(sol, labels = ["x" "y"], title = "Lotka Volterra")
+
+# 2. Getting gradients
+using DifferentiationInterface
+using ForwardDiff, FiniteDiff
+
+f(x) = sum(abs2, x)
+x = [1.0, 2.0]
+value_and_gradient(f, AutoFiniteDiff(), x)
+
+# 3. Getting gradients from our solution
+function cost(prob, p, data)
+    sol =solve(prob, Tsit5(), p = p)
+
+    loss = 0.0 
+    for (t,x) in data 
+        loss += sum(abs2, sol(t) - x)
+    end
+    return loss
+end
+
+data = [(2.0, [5.0, 2.0]), (6.0, [4.0, 4.0])]
+cost(prob, p, data)
+
+fnc = let prob=prob, data=data 
+    (p, theta) -> cost(prob, p, data)
+end
+
+@time value_and_gradient(fnc, AutoForwardDiff(), p)
+@time value_and_gradient(fnc, AutoFiniteDiff(), p)
+
+
+
+using Optimization
+
+optf = OptimizationFunction(fnc, AutoForwardDiff())
+optprob = OptimizationProblem(optf, p)
+optsol = solve(optprob, Optimization.LBFGS())
+
+
+
+sol_opt = solve(prob, Tsit5(), p = optsol.u)
+
+begin
+    plot(sol_opt, labels = ["x*" "y*"], color = [1 2])
+    scatter!(getindex.(data, 1), getindex.(data, 2), color = [1 2])
+    plot!(sol, labels = ["x" "y"], linestyle = :dash)
+end
